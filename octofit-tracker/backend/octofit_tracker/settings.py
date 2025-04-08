11f48d4cdbda63d8ff7ad57a58c5d1ff0a51c4e4
shar c <40021332+sharwren@users.noreply.github.com>
2025-04-08 12:51:32 +0000

    spare

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
+    },
+    'spare': {
+        'ENGINE': 'djongo',
+        'NAME': 'spare_db',
+        'HOST': 'localhost',
+        'PORT': 37017,
     }
 }
 
