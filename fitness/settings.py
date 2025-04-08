9e34d4e560b5baa7527c23891066c45b76535790
shar c <40021332+sharwren@users.noreply.github.com>
2025-04-08 12:48:46 +0000

    edit

--- fitness/settings.py
+++ fitness/settings.py
@@ -4,5 +4,11 @@ DATABASES = {
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
+    },
+    'spare': {
+        'ENGINE': 'djongo',
+        'NAME': 'spare_db',
+        'HOST': 'localhost',
+        'PORT': 37017,
     }
 }
